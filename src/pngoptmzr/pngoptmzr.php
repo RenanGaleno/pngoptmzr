04be7c23d4bdd32f39c367af8e5720a2e06aa495
Renan Galeno <renangaleno@gmail.com>
2015-08-28 07:12:51 -0300

    Libraries added

--- src/pngoptmzr/pngoptmzr.php
+++ src/pngoptmzr/pngoptmzr.php
@@ -2,7 +2,6 @@
 
 /*
  * This file is part of the pngoptmzr package.
- * Based on original code by pngquant: https://pngquant.org/php.html
  *
  * (c) Renan Galeno <g@renan.eng.br>
  *
@@ -14,26 +13,40 @@ namespace pngoptmzr;
 
 class pngoptmzr
 {
-	/* Check if png quant is installed
-	 * @return boolean - true if pngquant is installed, false if it isn't
-	*/
-	public static function isOk()
-	{
-		// Check if pngquant is installed
-		$sh = shell_exec("which pngquant");
-		// Return true or false
-	    return (empty($sh) ? false : true);
-	}
-	/* Optimizes PNG file with pngquant from a path
+	/* Optimizes PNG file from a path
 	 * @param $imagepath string - path to any PNG file, e.g. $_FILE['file']['tmp_name']
 	 * @param $max_quality int - conversion quality, useful values from 60 to 100 (smaller number = smaller file)
 	 * @param $min_quality int - guarantee that quality won't be worse than that
 	 * @return string - content of PNG file after conversion
 	*/
-	public static function fromFile($imagepath, $max_quality = 90, $min_quality = 60)
+	public static function fromFile($imagepath, $max_quality = 80, $min_quality = 60)
 	{
-		// Shell exec pngquant getting return as string
-		$compressed_png_content = shell_exec("pngquant --quality=$min_quality-$max_quality - < ".escapeshellarg($imagepath));
+		// PNGQuant
+		shell_exec("pngquant --quality=$min_quality-$max_quality -- ".escapeshellarg($imagepath));
+		// OptiPNG
+		shell_exec("optipng ".escapeshellarg($imagepath));
+		// PNGCrush
+		shell_exec("pngcrush -ow ".escapeshellarg($imagepath));
+
+		//Read from file
+		$compressed_png_content = file_get_contents($imagepath);
+	    // Return the string containing minified png
+	    return $compressed_png_content;
+	}
+
+	/* Optimizes PNG file from a string
+	 * @param $string string - contents of the PNG file
+	 * @param $max_quality int - conversion quality, useful values from 60 to 100 (smaller number = smaller file)
+	 * @param $min_quality int - guarantee that quality won't be worse than that
+	 * @return string - content of PNG file after conversion
+	*/
+	public static function fromString($string, $max_quality = 90, $min_quality = 60)
+	{
+		// Save the string in disk
+		$filename = "tmp".md5(uniqid(rand(), true)).".png";
+		file_put_contents($filename, $string);
+		$compressed_png_content = pngoptmzr::fromFile($filename, $max_quality, $min_quality);
+		unlink($filename);
 	    // Return the string containing minified png
 	    return $compressed_png_content;
 	}
